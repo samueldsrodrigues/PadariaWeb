ebcaabb27999f1a4f5718a411f3a259a0020b405
EchoTide <samucacwc@gmail.com>
2026-01-26 20:31:46 -0300

    Adicionando bean da página incluir funcionario

--- src/main/java/br/com/padariaweb/bean/FuncionarioIncluirBean.java
+++ src/main/java/br/com/padariaweb/bean/FuncionarioIncluirBean.java
@@ -1,7 +1,10 @@
 package br.com.padariaweb.bean;
 
 import java.io.Serializable;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -11,9 +14,11 @@ import javax.faces.context.FacesContext;
 
 import br.com.padariaweb.entity.Cargo;
 import br.com.padariaweb.entity.Funcionario;
+import br.com.padariaweb.exception.ValidacaoException;
 import br.com.padariaweb.service.ICargoService;
 import br.com.padariaweb.service.IFuncionarioService;
 import br.com.padariaweb.util.AbstractView;
+import br.com.padariaweb.util.Util;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -32,7 +37,6 @@ public class FuncionarioIncluirBean extends AbstractView implements Serializable
 	@ManagedProperty("#{cargoService}")
 	private @Setter ICargoService cargoService;
 
-
 	private @Getter @Setter Funcionario funcionario;
 
 	private @Getter @Setter String senhaAtual;
@@ -43,7 +47,7 @@ public class FuncionarioIncluirBean extends AbstractView implements Serializable
 
 	private @Getter @Setter List<Cargo> cargos;
 
-	private static final String URL_PAGINA = "/pages/admin/funcionario/incluir";
+	private static final String URL_PAGINA = "/pages/funcionario/incluir";
 
 	private @Getter @Setter boolean exibirFileUpload = false;
 	private @Getter @Setter boolean exibirWebCam = false;
@@ -51,8 +55,7 @@ public class FuncionarioIncluirBean extends AbstractView implements Serializable
 
 	@PostConstruct
 	public void init() {
-		
-		
+
 	}
 
 	public String incluir() {
@@ -60,45 +63,101 @@ public class FuncionarioIncluirBean extends AbstractView implements Serializable
 	}
 
 	public String salvar() {
-		// Se o FuncionarioLogado não for ROOT então o GrupoLoja do Funcionario a ser incluído
-		
+		try {
+			if (funcionario.getSqFuncionario() == null) {
+				if (funcionario.getNome() == null || funcionario.getNome().isEmpty()) {
+					addMsgError("Campo Nome é obrigatório");
+					return null;
+				}
+				if (funcionario.getCargo() == null) {
+					addMsgError("Campo Cargo é obrigatório");
+					return null;
+				}
+				if (!validEmailEasy(funcionario.getEmail())) {
+					addMsgError("Campo E-mail é inválido");
+					return null;
+				}
+				funcionarioService.salvar(funcionario);
+				addMsgInfo("Usuário salvo com sucesso");
+			} else {
+				if (funcionario.getNome() == null || funcionario.getNome().isEmpty()) {
+					addMsgError("Campo Nome é obrigatório");
+					return null;
+				}
+				if (!validEmailEasy(funcionario.getEmail())) {
+					addMsgError("Campo E-mail é inválido");
+					return null;
+				}
+				funcionarioService.salvar(funcionario);
+				addMsgInfo("Usuário alterado com sucesso");
+			}
 			return "listar";
-		
+		} catch (ValidacaoException e) {
+			addMsgError(e.getMessage());
+			return null;
+		}
 	}
 
-	public String salvarPrimeiroAcesso() {
-		if (senhaAtual == null || senhaAtual.isEmpty()) {
-			addMsgError("Campo Senha Atual é obrigatório");
-			return null;
-		}
-		if (senha == null || senha.isEmpty()) {
-			addMsgError("Campo Nova Senha é obrigatório");
-			return null;
-		}
-		if (confirmacaoSenha == null || confirmacaoSenha.isEmpty()) {
-			addMsgError("Campo Confirmar Senha é obrigatório");
-			return null;
-		}
+//	public void carregarListaLoja(){
+//		if(!Util.isValido(funcionario)){
+//			lojas = lojaService.pesquisarLojaByGrupoLoja(appBean.getFuncionarioLogado().getGrupoLoja());
+//		}else if(Util.isValido(funcionario.getGrupoLoja())){
+//			lojas = lojaService.pesquisarLojaByGrupoLoja(funcionario.getGrupoLoja());
+//		}
+//	}
 
-		if (!senha.equals(confirmacaoSenha)) {
-			addMsgError("A Nova Senha e a confirmação não são iguais.");
-			return null;
+	public boolean validEmail(String email) {
+		System.out.println("Metodo de validacao de email");
+//	    Pattern p = Pattern.compile("^[\\w-]+(\\.[\\w-]+)*@([\\w-]+\\.)+[a-zA-Z]{2,7}$");
+		String regex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@+([_A-Za-z0-9-])$";
+		Matcher m = Pattern.compile(regex).matcher(email.toLowerCase());
+		if (m.find()) {
+			System.out.println("O email " + email + " e valido");
+			return true;
+		} else {
+			System.out.println("O E-mail " + email + " é inválido");
+			return false;
 		}
-
-		
-		// invalidating session
-		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-
-		return "/login?faces-redirect=true";
 	}
 
+	public boolean validEmailEasy(String email) {
+		if (email.trim().contains("@")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-
+//	public String salvarPrimeiroAcesso() {
+//		if (senhaAtual == null || senhaAtual.isEmpty()) {
+//			addMsgError("Campo Senha Atual é obrigatório");
+//			return null;
+//		}
+//		if (senha == null || senha.isEmpty()) {
+//			addMsgError("Campo Nova Senha é obrigatório");
+//			return null;
+//		}
+//		if (confirmacaoSenha == null || confirmacaoSenha.isEmpty()) {
+//			addMsgError("Campo Confirmar Senha é obrigatório");
+//			return null;
+//		}
+//
+//		if (!senha.equals(confirmacaoSenha)) {
+//			addMsgError("A Nova Senha e a confirmação não são iguais.");
+//			return null;
+//		}
+//
+//		
+//		// invalidating session
+//		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+//
+//		return "/login?faces-redirect=true";
+//	}
 
 	public String alterarFuncionario() {
-		//funcionario = funcionarioService.pesquisarFuncionario(funcionarioAlteracao);
+		// funcionario = funcionarioService.pesquisarFuncionario(funcionarioAlteracao);
 
-		return "/pages/admin/funcionario/incluir";
+		return "/pages/funcionario/incluir";
 	}
 
 }
