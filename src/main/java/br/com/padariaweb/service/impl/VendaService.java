bf4d5ddcb6c96b4b5d34863f0cbd2cc54fadf809
EchoTide <samucacwc@gmail.com>
2026-06-16 08:31:58 -0300

    Venda Service

--- src/main/java/br/com/padariaweb/service/impl/VendaService.java
+++ src/main/java/br/com/padariaweb/service/impl/VendaService.java
@@ -1,13 +1,19 @@
 package br.com.padariaweb.service.impl;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.padariaweb.dao.IFormaPagamentoDao;
 import br.com.padariaweb.dao.IVendaDao;
+import br.com.padariaweb.dao.IVendaProdutoDao;
+import br.com.padariaweb.entity.FormaPagamento;
 import br.com.padariaweb.entity.Venda;
+import br.com.padariaweb.entity.VendaProduto;
 import br.com.padariaweb.exception.ValidacaoException;
 import br.com.padariaweb.service.IVendaService;
 
@@ -16,45 +22,56 @@ import br.com.padariaweb.service.IVendaService;
 public class VendaService extends GenericoCRUDManager<Venda, Long> implements IVendaService {
 
 	@Autowired
-	IVendaDao vendaDao;
+    private IVendaDao vendaDao;
+	
+	@Autowired
+    private IVendaProdutoDao vendaProdutoDao;
+	
+	@Autowired
+    private IFormaPagamentoDao formaPagamentoDao;
+
+    public List<Venda> pesquisarVenda(Venda filtro, Integer first, Integer maxPerPage) {
+        return vendaDao.pesquisarVenda(filtro, first, maxPerPage);
+    }
+
+    public void salvar(Venda venda) throws ValidacaoException {
+        if (venda.getDtVenda() == null) {
+            venda.setDtVenda(new Date());
+        }
+
+        if (venda.getVlTotal() == null) {
+            venda.setVlTotal(BigDecimal.ZERO);
+        }
+
+        if (venda.getSqVenda() != null)
+            vendaDao.update(venda);
+        else
+            vendaDao.save(venda);
+    }
 
 	@Override
-	public List<Venda> pesquisarVenda(Venda filtro) {
-		return vendaDao.verificaVendaExistente(filtro);
+	public void excluirVendaCompleta(Venda venda) throws ValidacaoException {
+
+	    VendaProduto filtroItem = new VendaProduto();
+	    filtroItem.setVenda(venda);
+
+	    List<VendaProduto> itens = vendaProdutoDao.pesquisarVendaProduto(filtroItem, null, 500);
+
+	    for (VendaProduto item : itens) {
+	        vendaProdutoDao.delete(item);
+	    }
+
+	    FormaPagamento filtroPagamento = new FormaPagamento();
+	    filtroPagamento.setVenda(venda);
+
+	    List<FormaPagamento> pagamentos = formaPagamentoDao.pesquisarFormaPagamento(filtroPagamento, null, 500);
+
+	    for (FormaPagamento pagamento : pagamentos) {
+	        formaPagamentoDao.delete(pagamento);
+	    }
+
+	    vendaDao.delete(venda);
 	}
-
-	@Override
-	public List<Venda> pesquisarVenda(Venda filtro, Integer first, Integer maxPerPage) {
-
-		return vendaDao.pesquisarVenda(filtro, first, maxPerPage);
-	}
-
-	public void salvar(Venda venda) throws ValidacaoException {
-		List<Venda> vendas = vendaDao.verificaVendaExistente(venda);
-		// Caso seja inclusao de um novo venda
-		if ((venda.getSqVenda() == null && !vendas.isEmpty())
-				// Caso seja alteracao de venda
-				|| (venda.getSqVenda() != null && !vendas.isEmpty()
-						&& !venda.getSqVenda().equals(vendas.get(0).getSqVenda())))
-			throw new ValidacaoException("Venda já cadastrada na base de dados.");
-
-		
-
-		if (venda.getSqVenda() != null)
-			vendaDao.update(venda);
-		else
-			vendaDao.save(venda);
-	}
-
-	public Venda pesquisarVenda(Long vendaAlteracao) {
-		Venda u = (Venda) vendaDao.findById(Venda.class, vendaAlteracao);
-		vendaDao.evict(u);
-		return u;
-	}
-
-	public void inativarVenda(Venda vendaInativar) {
-		vendaDao.save(vendaInativar);
-
-	}
-
 }
+
+
